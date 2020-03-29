125be57107c271ebdc63aac9ad01ad24d2c7d300
loc-nt <locnt.plp@gmail.com>
2020-03-29 13:44:14 -0700

    git bash commit

---
+++ PyPoll.py
@@ -0,0 +1,25 @@
+# Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources\election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # To do: read and analyze the data here. 
+    # Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+
+    # Print the header row.
+    headers = next(file_reader)
+    print(headers)
+
+# Total number of votes cast
+# 1. A complete list of candidates who received votes
+# 2. Total number of votes each candidate received
+# 3. Percentage of votes each candidate won
+# 4. The winner of the election based on popular vote
+
